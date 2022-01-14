bc87a1e62e2c64dd5426453669d2957e7bdafd0c
Paul Sicking <sickingp@gmail.com>
2022-01-13 19:17:58 -0800

    First iteration of autonomous orchestration

--- autonomous.java
+++ autonomous.java
@@ -28,6 +28,8 @@ public class Autonomous extends LinearOpMode {
   public void runOpMode() {
     double motorpos;
     double newmotorpos;
+    int pos = 1;
+    int level = 0;
     
     cdv3_DistanceSensor = hardwareMap.get(DistanceSensor.class, "cdv3");
     cdv3 = hardwareMap.get(ColorSensor.class, "cdv3");
@@ -44,21 +46,95 @@ public class Autonomous extends LinearOpMode {
     if (opModeIsActive()) {
       // Put run blocks here
       motorpos = rot.getCurrentPosition();
-      newmotorpos = motorpos - 440;
+      newmotorpos = motorpos - 470;
       
-      claw.setPosition(0);
-      sleep(1000);
-      rightpos(newmotorpos);
-      sleep(1000);
-      l2();
-      sleep(1000);
-      forward(100);
-      sleep(100);
-      claw.setPosition(1);
-      sleep(1000);
-      leftpos(motorpos);
-      claw.setPosition(0);
-      back(100);
+      if (pos == 1){
+        forward(200);
+        sleep(100);
+        right(580);
+        sleep(100);
+        back(490);
+        sleep(1000);
+        ducksgo();
+        sleep(5600);
+        ducksstop();
+        sleep(200);
+        forward(230);
+        sleep(100);
+        left(600);
+        sleep(100);
+        forward(125);
+        sleep(500);
+        if(cdv3_DistanceSensor.getDistance(DistanceUnit.CM) < 3.8){
+          level = 1;
+        }
+        sleep(200);
+        straferight(300);
+        sleep(200);
+        if(cdv3_DistanceSensor.getDistance(DistanceUnit.CM) < 3.8){
+          level = 2;
+        }
+        sleep(200);
+        straferight(300);
+        sleep(200);
+        sleep(100);
+        if(cdv3_DistanceSensor.getDistance(DistanceUnit.CM) < 3.8){
+          level = 3;
+        }
+        sleep(200);
+        straferight(480);
+        sleep(500);
+        if(level == 1){
+          straferight(250);
+          sleep(100);
+          rightpos(newmotorpos);
+          sleep(500);
+          forward(100);
+          sleep(200);
+          claw.setPosition(1);
+          sleep(3000);
+          claw.setPosition(0.5);
+        }else if(level == 2){
+          l2();
+          sleep(1000);
+          straferight(250);
+          sleep(100);
+          rightpos(newmotorpos);
+          sleep(500);
+          forward(100);
+          sleep(200);
+          claw.setPosition(1);
+          sleep(3000);
+          claw.setPosition(0.5);
+        }else if (level == 3){
+          l3();
+          sleep(200);
+          straferight(250);
+          sleep(100);
+          rightpos(newmotorpos);
+          sleep(500);
+          forward(150);
+          sleep(200);
+          claw.setPosition(1);
+          sleep(3000);
+          claw.setPosition(0.5);
+        }
+        back(300);
+        left(500);
+        forward(1200);
+        ducksstop();
+        
+      }else if(pos == 4){
+        forward(150);
+        sleep(100);
+        straferight(900);
+        duckie.setPosition(1);
+        sleep(5800);
+        ducksstop();
+        forward(500);
+      
+        
+      }
       
       
       
@@ -68,6 +144,7 @@ public class Autonomous extends LinearOpMode {
         
         telemetry.addData("Distance Inch", cdv3_DistanceSensor.getDistance(DistanceUnit.CM));
         telemetry.addData("red", cdv3.red());
+        telemetry.addData("level", level);
         telemetry.addData("motorpos", newmotorpos);
         telemetry.update();
       }
@@ -75,44 +152,53 @@ public class Autonomous extends LinearOpMode {
   }
   //Movement
   private void forward(long t) {
-    BL.setPower(1);
-    BR.setPower(-1);
-    FL.setPower(1);
-    FR.setPower(-1);
+    BL.setPower(0.5);
+    BR.setPower(-0.5);
+    FL.setPower(0.5);
+    FR.setPower(-0.5);
     sleep(t);
     stopmove();
   }
   private void back(long t) {
-    BL.setPower(-1);
-    BR.setPower(1);
-    FL.setPower(-1);
-    FR.setPower(1);
+    BL.setPower(-0.5);
+    BR.setPower(0.5);
+    FL.setPower(-0.5);
+    FR.setPower(0.5);
     sleep(t);
     stopmove();
   }
-  private void left() {
-    BL.setPower(-1);
-    BR.setPower(-1);
-    FL.setPower(-1);
-    FR.setPower(-1);
+  private void left(long t) {
+    BL.setPower(-0.5);
+    BR.setPower(-0.5);
+    FL.setPower(-0.5);
+    FR.setPower(-0.5);
+    sleep(t);
+    stopmove();
   }
-  private void right() {
-    BL.setPower(1);
-    BR.setPower(1);
-    FL.setPower(1);
-    FR.setPower(1);
+  private void right(long t) {
+    BL.setPower(0.5);
+    BR.setPower(0.5);
+    FL.setPower(0.5);
+    FR.setPower(0.5);
+    sleep(t);
+    stopmove();
   }
-  private void strafeleft() {
-    BL.setPower(-1);
-    BR.setPower(1);
-    FL.setPower(1);
-    FR.setPower(-1);
+  private void strafeleft(long t) {
+    BL.setPower(-0.5);
+    BR.setPower(0.5);
+    FL.setPower(0.5);
+    FR.setPower(-0.5);
+    sleep(t);
+    stopmove();
+    
   }
-  private void straferight() {
-    BL.setPower(1);
-    BR.setPower(-1);
-    FL.setPower(-1);
-    FR.setPower(1);
+  private void straferight(long t) {
+    BL.setPower(0.5);
+    BR.setPower(-0.5);
+    FL.setPower(-0.5);
+    FR.setPower(0.5);
+    sleep(t);
+    stopmove();
   }
   private void stopmove() {
     BL.setPower(0);
@@ -122,7 +208,7 @@ public class Autonomous extends LinearOpMode {
   }
   //Ducks
   private void ducksgo() {
-    duckie.setPosition(1);
+    duckie.setPosition(0);
   }
   private void ducksstop(){
     duckie.setPosition(0.5);
@@ -139,12 +225,12 @@ public class Autonomous extends LinearOpMode {
   }
   private void l2(){
     up();
-    sleep(450);
+    sleep(550);
     stopvert();
   }
   private void l3(){
     up();
-    sleep(1500);
+    sleep(2000);
     stopvert();
   }
   //Rotation
